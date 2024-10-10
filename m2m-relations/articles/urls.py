bf917fd166dbca1713cf1cd68019796f53715949
VitZur <131258965+VitZur@users.noreply.github.com>
2024-10-10 17:08:39 +0300

    Add files via upload

---
+++ m2m-relations/articles/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include,path
+
+from articles.views import articles_list
+from django.conf import settings
+
+urlpatterns = [
+    path('', articles_list, name='articles'),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns =[
+        path('__debug__/',include(debug_toolbar.urls))
+    ] + urlpatterns
